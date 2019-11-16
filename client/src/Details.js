ee595f0af0afcc495f559919f9032525962c5919
Twisha <twishask@gmail.com>
2019-11-16 11:56:54 +0530

    Details fixed

--- client/src/Details.js
+++ client/src/Details.js
@@ -5,13 +5,19 @@ function Details(props) {
 
   const details = props.location.state.details
   const stringDetails = JSON.stringify(details)
-  var formattedString = stringDetails.split(",").join(",\n")
+  var formattedString = stringDetails.split(",")
   return (
     <div className="App">
     <headers className="App-header">
     <h3>Order details</h3>
     <div className="details">
-    {formattedString}
+    {formattedString.map(item => {
+      return(
+        <div>
+        {item},
+        </div>
+      )
+    })}
     </div>
     </headers>
     </div>
